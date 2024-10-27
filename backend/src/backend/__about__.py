fd5976cfd5ea12ae03147c5ad8c73dd57b95de0e
Ron M <ramayer+git@gmail.com>
2024-10-27 16:39:41 -0700

    added backend using `hatch new backend`

---
+++ backend/src/backend/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2024-present Ron M <ramayer+git@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ backend/src/backend/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2024-present Ron M <ramayer+git@gmail.com>
+#
+# SPDX-License-Identifier: MIT
